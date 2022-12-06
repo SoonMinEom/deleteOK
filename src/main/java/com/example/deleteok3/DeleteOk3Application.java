d6e663f3a7fb4a4b66bb9123eab8cf9ba77c3ca0
soonmineom <skdlfma123@gmail.com>
2022-12-06 11:25:52 +0900

    프로젝트 빌드

---
+++ src/main/java/com/example/deleteok3/DeleteOk3Application.java
@@ -0,0 +1,13 @@
+package com.example.deleteok3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleteOk3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleteOk3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deleteok3/DeleteOk3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deleteok3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeleteOk3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
